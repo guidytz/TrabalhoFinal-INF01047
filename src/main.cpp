08ab2d36e763a64216870aa0c202a0a9620ff8b2
Guilherme Dytz <guidytz@gmail.com>
2021-05-14 11:52:57 -0300

    Considerando um zoom para a câmera look-at

--- src/main.cpp
+++ src/main.cpp
@@ -1333,30 +1333,22 @@ void ScrollCallback(GLFWwindow* window, double xoffset, double yoffset)
 {
     // // Atualizamos a distância da câmera para a origem utilizando a
     // // movimentação da "rodinha", simulando um ZOOM.
-    // g_CameraDistance -= 0.1f*yoffset;
+    g_CameraDistance -= 0.2f*yoffset;
 
     // // Uma câmera look-at nunca pode estar exatamente "em cima" do ponto para
     // // onde ela está olhando, pois isto gera problemas de divisão por zero na
     // // definição do sistema de coordenadas da câmera. Isto é, a variável abaixo
     // // nunca pode ser zero. Versões anteriores deste código possuíam este bug,
     // // o qual foi detectado pelo aluno Vinicius Fraga (2017/2).
-    // const float verysmallnumber = std::numeric_limits<float>::epsilon();
-    // if (g_CameraDistance < verysmallnumber)
-    //     g_CameraDistance = verysmallnumber;
+    const float verysmallnumber = std::numeric_limits<float>::epsilon();
+    if (g_CameraDistance < verysmallnumber)
+        g_CameraDistance = verysmallnumber;
 }
 
 // Definição da função que será chamada sempre que o usuário pressionar alguma
 // tecla do teclado. Veja http://www.glfw.org/docs/latest/input_guide.html#input_key
 void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mod)
 {
-    // ==============
-    // Não modifique este loop! Ele é utilizando para correção automatizada dos
-    // laboratórios. Deve ser sempre o primeiro comando desta função KeyCallback().
-    for (int i = 0; i < 10; ++i)
-        if (key == GLFW_KEY_0 + i && action == GLFW_PRESS && mod == GLFW_MOD_SHIFT)
-            std::exit(100 + i);
-    // ==============
-
     // Se o usuário pressionar a tecla ESC, fechamos a janela.
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GL_TRUE);
@@ -1379,9 +1371,6 @@ void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mod)
     if (key == GLFW_KEY_R && action == GLFW_PRESS)
     {
         resetGame();
-        // LoadShadersFromFiles();
-        // fprintf(stdout,"Shaders recarregados!\n");
-        // fflush(stdout);
     }
 
      if (key == GLFW_KEY_W){ // Movimento para frente
